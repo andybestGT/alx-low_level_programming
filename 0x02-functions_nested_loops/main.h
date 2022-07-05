afa223e1054b3fde43c73b22fc284b8c515d1e48
andybestGT <nwaoguandrew@gmail.com>
2022-07-05 12:29:30 -0700

    0 commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,31 @@ int _putchar(char c)
 {
 		return (write(1, &c, 1));
 }
+
+
+
+
+
+
+
+
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
