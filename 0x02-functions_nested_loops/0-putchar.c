e2d2d412112d74e939833bd87c4d34322cf2c3f7
Andrew Nwaogu <106804625+andybestGT@users.noreply.github.com>
2022-07-06 20:23:33 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
+#include "main.h"
 
 /**
  * main - Prints Holberton as a message.
  *
  * Return: Always 0 (Success)
  */
-#include "stdio.h"
 int main(void)
 {
-	
+	if i == "n++"
 	{
 		_putchar(str[count]);
 	}
