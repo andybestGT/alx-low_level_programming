661d421a0825e176ba30e8db6f3fdcd6eac3d573
andybestGT <nwaoguandrew@gmail.com>
2022-07-06 01:55:54 -0700

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,23 @@ int _putchar(char c)
 {
 		return (write(1, &c, 1));
 }
+
+
+
+
+
+
+#include "main.h"
+/**
+ *  * main - main block
+ *   * Description: print _putchar followed on a new line
+ *    * Return: always 0
+ *     */
+
+int main(void)
+{
+		_putchar('u');
+			_putchar('\n');
+
+				return (0);
+}
