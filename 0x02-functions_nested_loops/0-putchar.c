6bcc57fb4d3f6cb14bcbeb7895f4ed115f22b0a5
andybestGT <nwaoguandrew@gmail.com>
2022-07-07 06:13:58 -0700

    correct o commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
- * main - Prints Holberton as a message.
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: print _putchar followed on a new line
+ * Return: always 0
  */
 int main(void)
 {
-	("_putchar(str[count])");
-	"(_putchar('\n'))";
-	
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+											return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,80 +1,9 @@
-#include <main.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
+int _putchar(char c);
+void 0-putchar.c(void);
 
 
 
-
-
-
-#include "main.h"
-/**
- *  * main - main block
- *   * Description: print _putchar followed on a new line
- *    * Return: always 0
- *     */
-
-int main(void)
-{
-		_putchar('u');
-			_putchar('\n');
-
-				return (0);
-}
-
-
-
-
-
-
-
-
-
-
-#include "main.h"
-
-/**
- *  * print_alphabet - entry point
- *   * Return: 0 Success
- *    */
-void print_alphabet(void)
-{
-		char j;
-
-			for (j = 'a'; j <= 'z'; j++)
-					{
-								_putchar(j);
-									}
-				_putchar('\n');
-}
-
-
-
-
-
-
-
-#include "main.h"
-
-/**
- *  * main - Prints Holberton as a message.
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		_putchar('r');
-		_putchar('\n');
-				
-		return (0);
-}
+#endif
