8ee7541bd7473eb903e761e7f4115e1d3c24d82e
andybestGT <nwaoguandrew@gmail.com>
2022-07-07 02:22:49 -0700

    0 commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -57,3 +57,24 @@ void print_alphabet(void)
 									}
 				_putchar('\n');
 }
+
+
+
+
+
+
+
+#include "main.h"
+
+/**
+ *  * main - Prints Holberton as a message.
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		_putchar('r');
+		_putchar('\n');
+				
+		return (0);
+}
