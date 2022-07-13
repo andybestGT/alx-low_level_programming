dc4d6b7f058b993f6481648edef2552cc0e0476e
Andrew Nwaogu <106804625+andybestGT@users.noreply.github.com>
2022-07-13 15:58:39 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,11 @@
 
 void _puts(char *str)
 {
-		int i;
+	int i;
 
-		for (i = 0; str[i] != 0; i++)
+	for (i = 0; str[i] != 0; i++)
 	{
-  			_putchar(str[i]);
+  		_putchar(str[i]);
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
