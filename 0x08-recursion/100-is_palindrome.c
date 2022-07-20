e0cca4c153b7d5bef74625c86e1e5a99e4f28ba6
Andrew Nwaogu <106804625+andybestGT@users.noreply.github.com>
2022-07-20 21:29:14 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -42,14 +42,3 @@ int is_palindrome(char *s)
 		return (1);
 	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
-/**
- *  * is_palindrome - detects if a string is a palindrome.
- *   * @s: string.
- *    * Return: 1 if s is a palindrome, 0 if not.
- *     */
-int is_palindrome(char *s)
-{
-		if (*s == '\0')
-					return (1);
-			return (comparator(s, 0, _strlen_recursion(s) - 1));
-}
